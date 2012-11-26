b015d1ad2f3a48cee66ef5b793fc19d772eb76a1
Linus Lundahl <lundahl.linus@gmail.com>
2012-11-26 14:00:58 +0100

    Initial Commit.

---
+++ PhpSyntaxSmeller.py
@@ -0,0 +1,32 @@
+import sublime
+import sublime_plugin
+import os
+import re
+
+
+class PhpSyntaxChecker(sublime_plugin.EventListener):
+    TARGET_SUFFIXES = [".php", ".module", ".inc"]
+
+    def on_post_save(self, view):
+        settings = sublime.load_settings("PhpSyntaxChecker.sublime-settings").get("smelly")
+        path = view.file_name()
+        root, extension = os.path.splitext(path)
+
+        if extension in self.TARGET_SUFFIXES:
+            code = open(path, 'r').read()
+
+            ignore = re.compile('\/\/\slegitimate')
+
+            l = 1
+            errors = ''
+            for line in code.split("\n"):
+                for smell in settings:
+                    ex = settings[smell]
+                    ex = re.compile(ex)
+                    if ex.search(line) and not ignore.search(line):
+                        errors += "%s smells bad at line %i\n" % (smell, l)
+                l = l + 1
+
+            if len(errors):
+                # sublime.error_message(errors)
+                os.system('terminal-notifier -message "' + errors + '"')
---
+++ README.md
@@ -0,0 +1,14 @@
+# PHP Syntax Smeller for Sublime Text 2
+
+Sublime Text 2 plugin that checks PHP files for "smelly" code such as `die;`, `var_dump();`, `print_r();` etc.
+
+Installation
+------------
+	$ git clone git@github.com:linuslundahl/PHP-Syntax-Smeller.git ~/Library/Application Support/Sublime Text 2/Packages
+
+Requirements
+------------
+
+Requires [Terminal Notifier](https://github.com/alloy/terminal-notifier).
+
+	$ [sudo] gem install terminal-notifier
