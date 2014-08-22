069bf308eb9a2c026de1e6a83e11e8db76260323
Linus Lundahl <lundahl.linus@gmail.com>
2014-08-22 15:10:01 +0200

    Python 3 fixes.

--- PhpDevelSmeller.py
+++ PhpDevelSmeller.py
@@ -6,27 +6,28 @@ import re
 
 class PhpDevelSmeller(sublime_plugin.EventListener):
     def on_post_save(self, view):
+        """Finds and alerts the user of smelly PHP code."""
         settings = sublime.load_settings("PhpDevelSmeller.sublime-settings")
         path = view.file_name()
-        root, extension = os.path.splitext(path)
+        extension = os.path.splitext(path)[1]
 
         if extension in settings.get("extensions"):
             smells = settings.get("smells")
-            code = open(path, 'r').read()
-            ignore = re.compile('\/\/\slegitimate')
+            code = open(path, 'r', encoding='utf-8').read()
+            ignore = re.compile('\/\/\slegitimate\r')
 
-            l = 1
+            i = 1
             errors = []
             for line in code.split("\n"):
                 for smell in smells:
                     ex = re.compile(smells[smell])
                     if ex.search(line) and not ignore.search(line):
-                        errors.append("%s:%i" % (smell, l))
-                l = l + 1
+                        errors.append("%s:%i" % (smell, i))
+                i = i + 1
 
             if len(errors):
                 errors = ', '.join(errors)
-                if (settings.get("use_notification_center")):
+                if settings.get("use_notification_center"):
                     os.system('terminal-notifier -message "' + errors + '"')
                 else:
                     sublime.error_message(errors)
@@ -34,26 +35,27 @@ class PhpDevelSmeller(sublime_plugin.EventListener):
 
 class PhpDevelCleanerCommand(sublime_plugin.TextCommand):
     def run(self, edit):
+        """Cleans the current file from smelly code."""
         settings = sublime.load_settings("PhpDevelSmeller.sublime-settings")
         path = self.view.file_name()
-        root, extension = os.path.splitext(path)
+        extension = os.path.splitext(path)[1]
 
         if extension in settings.get("extensions"):
             smells = settings.get("smells")
-            ignore = re.compile('\/\/\slegitimate')
+            ignore = re.compile('\/\/\slegitimate\r')
 
             edit = self.view.begin_edit()
             for smell in smells:
                 offset = 0
                 i = 0
-                r = []
+                arr = []
                 for region in self.view.find_all("^.*(" + smells[smell] + ".*;)\\n?$"):
                     line = self.view.substr(self.view.line(region))
                     if not ignore.search(line):
-                        r.append(sublime.Region(region.begin() + offset, region.end() + offset))
-                        offset -= r[i].size()
+                        arr.append(sublime.Region(region.begin() + offset, region.end() + offset))
+                        offset -= arr[i].size()
                         i = i + 1
 
-                for i in r:
+                for i in arr:
                     self.view.erase(edit, i)
             self.view.end_edit(edit)
